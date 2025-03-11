cfd8cbbb47dd2fe25fa4a2d9070c9b55aff20f3c
fdcoull <58175445+fdcoull@users.noreply.github.com>
2025-03-11 16:11:08 +0000

    Add blockchain read test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
+import { Web3Provider } from '@ethersproject/providers';
 
 // Components - For page sections
 // import Navigation from './components/Navigation'
@@ -10,10 +11,39 @@ import { ethers } from 'ethers'
 // Config
 // import config from './config.json'
 
+// Test contract
+import Test from './abis/Test.json';
+import config from './config.json';
+
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [name, setName] = useState('');
+  
+  const loadBlockchainData = async () => {
+    // Connect to metamask wallet
+    const provider = new Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    // Get network and set contract
+    const network = await provider.getNetwork();
+    const contractAddress = config[network.chainId].test.address;
+    const contract = new ethers.Contract(contractAddress, Test, provider);
+    setContract(contract);
+
+    // Get name variable from contract
+    const name = await contract.name();
+    setName(name);
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div>
       <h2>Welcome to 0xPipeline!</h2>
+      <h3>Contract name: {name}</h3>
     </div>
   );
 }
