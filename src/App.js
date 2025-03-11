09abd340fab73a1aeeb248899273cc3c231bf3f9
fdcoull <58175445+fdcoull@users.noreply.github.com>
2025-03-11 14:34:30 +0000

    Setup React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components - For page sections
+// import Navigation from './components/Navigation'
+
+// ABIs
+// import Pipeline from './abis/Pipeline.json'
+
+// Config
+// import config from './config.json'
+
+function App() {
+  return (
+    <div>
+      <h2>Welcome to 0xPipeline!</h2>
+    </div>
+  );
+}
+
+export default App;
