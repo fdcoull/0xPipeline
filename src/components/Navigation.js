bdaef7a026459a877b55784d979af2e08ea2a89b
fdcoull <58175445+fdcoull@users.noreply.github.com>
2025-03-24 22:14:37 +0000

    Rearrange menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,27 +10,33 @@ const Navigation = ({ setView, view }) => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
+            <Nav className="me-auto">
+              {view === "material" && (
+                <>
+                  <Navbar.Brand onClick={() => setView("home")}><img src="0xP-Fabricate.png" alt="0xPipeline Logo" width="50"/></Navbar.Brand>
+                  
+                  <Nav.Link onClick={() => setView("home")}>Home</Nav.Link>
+                  <Nav.Link onClick={() => setView("home")}>Test1</Nav.Link>
+                </>
+              )}
+              {view === "fabricate" && (
+                <>
+                  <Navbar.Brand onClick={() => setView("home")}><img src="logo-full.png" alt="0xPipeline Logo" width="100"/></Navbar.Brand>
+                  <Nav.Link onClick={() => setView("home")}>Home</Nav.Link>
+                  <Nav.Link onClick={() => setView("home")}>Test2</Nav.Link>
+                </>
+              )}
+              {view === "transport" && (
+                <>
+                  <Navbar.Brand onClick={() => setView("home")}><img src="logo-full.png" alt="0xPipeline Logo" width="100"/></Navbar.Brand>
+                  <Nav.Link onClick={() => setView("home")}>Home</Nav.Link>
+                  <Nav.Link onClick={() => setView("home")}>Test3</Nav.Link>
+                </>
+              )}
+            </Nav>
+            <Nav>
               <Navbar.Brand onClick={() => setView("home")}><img src="logo-full.png" alt="0xPipeline Logo" width="100"/></Navbar.Brand>
-              <Nav className="me-auto">
-                {view === "material" && (
-                  <>
-                    <Nav.Link onClick={() => setView("home")}>Home</Nav.Link>
-                    <Nav.Link onClick={() => setView("home")}>Test1</Nav.Link>
-                  </>
-                )}
-                {view === "fabricate" && (
-                  <>
-                    <Nav.Link onClick={() => setView("home")}>Home</Nav.Link>
-                    <Nav.Link onClick={() => setView("home")}>Test2</Nav.Link>
-                  </>
-                )}
-                {view === "transport" && (
-                  <>
-                    <Nav.Link onClick={() => setView("home")}>Home</Nav.Link>
-                    <Nav.Link onClick={() => setView("home")}>Test3</Nav.Link>
-                  </>
-                )}
-              </Nav>
+            </Nav>
           </Container>
         </Navbar>
     );
