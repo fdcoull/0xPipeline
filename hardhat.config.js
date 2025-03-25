b846369517e66ee1337481472abbfc70ef39241b
fdcoull <58175445+fdcoull@users.noreply.github.com>
2025-03-25 23:25:10 +0000

    Add env require

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+    },
+  },
 };
