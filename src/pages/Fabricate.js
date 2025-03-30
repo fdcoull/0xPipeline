a9ec74f77b8d51cb2b0dec45ebe8e62f46dcca73
fdcoull <58175445+fdcoull@users.noreply.github.com>
2025-03-30 17:01:31 +0100

    Add par addition feature

--- src/pages/Fabricate.js
+++ src/pages/Fabricate.js
@@ -7,42 +7,89 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+
 
 const Fabricate = ({ setView, view, account, loadBlockchainData, contract }) => {
     const [parts, setParts] = useState([]);
+
+    // Add part state
+    const [showAddPart, setShowAddPart] = useState(false); 
+    const [isAddPartSubmitting, setIsAddPartSubmitting] = useState(false);
+    const [addPartName, setAddPartName] = useState('');
+    const [addPartQuantity, setAddPartQuantity] = useState(0);
+    const [addPartQuantityUnit, setAddPartQuantityUnit] = useState('');
+    const [addPartCost, setAddPartCost] = useState(0);
+    const isAddPartValid = () => {
+        return (
+            addPartName.trim() !== '' &&
+            !isNaN(Number(addPartQuantity)) &&
+            addPartQuantityUnit.trim() !== '' &&
+            !isNaN(Number(addPartCost))
+        );
+    };
     
-        const loadContractData = async () => {
-            if (contract) {
-                try {
-                    const loadedParts = [];
-                    
-                    const partCount = await contract.partCount();
-        
-                    for (let i = 1; i <= partCount; i++) {
-                        const [name, quantity, quantity_unit, cost] = await contract.parts(i);
-                        loadedParts.push({
-                            id: i.toString(),
-                            name: name,
-                            quantity: quantity.toString(),
-                            quantity_unit: quantity_unit,
-                            cost: cost.toString()
-                        });
-                    }
-        
-                    setParts(loadedParts);
-                } catch (error) {
-                    console.error("Error loading parts:", error);
+    const loadContractData = async () => {
+        if (contract) {
+            try {
+                const loadedParts = [];
+                
+                const partCount = await contract.partCount();
+    
+                for (let i = 1; i <= partCount; i++) {
+                    const [name, quantity, quantity_unit, cost] = await contract.parts(i);
+                    loadedParts.push({
+                        id: i.toString(),
+                        name: name,
+                        quantity: quantity.toString(),
+                        quantity_unit: quantity_unit,
+                        cost: cost.toString()
+                    });
                 }
+    
+                setParts(loadedParts);
+            } catch (error) {
+                console.error("Error loading parts:", error);
             }
         }
-    
-        useEffect(() => {
+    }
+
+    // Post list material form to blockchain
+    const postAddPart = async () => {
+        if (!contract) return;
+
+        setIsAddPartSubmitting(true);
+
+        try {
+            const transaction = await contract.addNewPart(
+                addPartName,
+                BigInt(addPartQuantity),
+                addPartQuantityUnit,
+                BigInt(addPartCost)
+            )
+
+            setShowAddPart(false);
             loadContractData();
-        }, [contract]);
+        } catch (err){
+            console.error("Add part failed:", err);
+        } finally {
+            setIsAddPartSubmitting(false);
+        }
+    }
+    
+    useEffect(() => {
+        loadContractData();
+    }, [contract]);
+
     return (
         <Container fluid>
             <h2>Parts</h2>
-            <Toolbar setView={setView} view={view}/>
+            <Nav.Item>
+                <Button variant="danger" onClick={() => setShowAddPart(true)}>Add Part</Button>
+            </Nav.Item>
             {parts.length > 0 ? (
             <Table striped bordered hover>
                 <thead>
@@ -69,6 +116,36 @@ const Fabricate = ({ setView, view, account, loadBlockchainData, contract }) => 
             ) : (
             <p>Nothing to show.</p>
             )}
+            <Modal show={showAddPart} onHide={() => setShowAddPart(false)} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Part</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" onChange={(e) => setAddPartName(e.target.value)}></Form.Control>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formQuantity">
+                            <Form.Label>Initial Quantity</Form.Label>
+                            <Form.Control type="number" onChange={(e) => setAddPartQuantity(e.target.value)}></Form.Control>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formQuantityUnit">
+                            <Form.Label>Quantity Unit</Form.Label>
+                            <Form.Control type="text" onChange={(e) => setAddPartQuantityUnit(e.target.value)}></Form.Control>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formCost">
+                            <Form.Label>Cost Per Unit</Form.Label>
+                            <Form.Control type="number" onChange={(e) => setAddPartCost(e.target.value)}></Form.Control>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowAddPart(false)}>Close</Button>
+                    <Button variant="primary" disabled={!isAddPartValid() || isAddPartSubmitting} onClick={postAddPart}>Save</Button>
+                    {isAddPartSubmitting ? "Processing..." : "Add a new part"}
+                </Modal.Footer>
+            </Modal>
         </Container>
     );
 }
