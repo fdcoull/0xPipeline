ff893f72176dc7d050b25d1169104b0da5ffb1f1
fdcoull <58175445+fdcoull@users.noreply.github.com>
2025-04-01 16:46:16 +0100

    Add fabricate providers list

--- src/pages/AccountFabricate.js
+++ src/pages/AccountFabricate.js
@@ -12,10 +12,88 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 
 // ABI
-import MaterialControl from '../abis/MaterialControl.json';
+import FabricateControl from '../abis/MaterialControl.json';
 
 const AccountMaterial = ({ setView, account, loadBlockchainData, pipelineContract, signer, fabricateContract }) => {
-    return "test";
+    const [providerProducts, setProviderProducts] = useState([]);
+    
+    const loadMaterialData = async () => {
+        if (!pipelineContract || !signer) return;
+
+        try {
+            const fabricateProviders = await pipelineContract.getAllFabricateProviders();
+            const providerProductsArray = [];
+
+            // Loop through providers
+            for (const provider of fabricateProviders) {
+                const fabricateContract = new ethers.Contract(provider, FabricateControl, signer);
+
+                const productCount = await fabricateContract.productCount();
+
+                const products = [];
+
+                // Loop through products
+                for (let i = 1; i <= productCount; i++) {
+                    const product = await fabricateContract.products(i);
+
+                    products.push({
+                        id: i.toString(),
+                        name: product.name,
+                        quantity: product.quantity.toString(),
+                        quantity_unit: product.quantity_unit,
+                        cost: product.cost.toString(),
+                    });
+                }
+
+                providerProductsArray.push({ provider, products })
+            }
+
+            setProviderProducts(providerProductsArray);
+            
+        } catch (err) {
+            console.error("Error loading provider data:", err);
+        }
+    }
+
+    useEffect(() => {
+            if (signer && pipelineContract) {
+                loadMaterialData();
+            }
+    }, []);
+    
+    return (
+        <Container fluid>
+            <h2>Fabricate Providers</h2>
+            {providerProducts.map((providerData) => (
+            <div key={providerData.provider}>
+                <h4>{providerData.provider}</h4>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>QTY</th>
+                            <th>QTY Unit</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {providerData.products.map((product) => (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td>{product.name}</td>
+                                <td>{product.quantity}</td>
+                                <td>{product.quantity_unit}</td>
+                                <td>{product.cost}</td>
+                                <td><Button variant="warning" onClick={() => {setSelectedProvider(providerData.provider); setSelectedProduct(product); setQuantity(1); setShowBuyModal(true);}}>Buy</Button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
+        ))}
+        </Container>
+    );
 }
 
 export default AccountMaterial;
